31527dbd1bc358cf227438cd97cb0d475eb1fa5f
Maurizio Tomasi <maurizio.tomasi@unimi.it>
2017-12-01 12:24:01 +0100

    Fix DATE_INPUT_FORMATS

--- stdb2/settings.py
+++ stdb2/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-DATE_INPUT_FORMATS = '%Y-%m-%d'
+DATE_INPUT_FORMATS = ('%Y-%m-%d',)
 
 LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
 
